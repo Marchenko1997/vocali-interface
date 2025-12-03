cfee8b22730672f9d678b3af77b37886656b6e7e
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-12-03 16:14:47 +0100

    set up main pages

---
+++ my-app/src/pages/Auth.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'      
+import { useSelector } from 'react-redux'            
+import Login from './Login'
+import Register from './Register'
+import Confirmation from './Confirmation'
+import type { RootState } from '../redux/store'
+
+
+
+const Auth = () => {
+    const location = useLocation()
+   const [currentView, setCurrentView] = useState<"login" | "register">(
+     "login"
+    );
+    
+    const { needConfirmation } = useSelector((state: RootState) => state.auth);
+
+    useEffect(() => { 
+        const hash = location.hash.replace("#", "");
+        if (hash === "register") {
+            setCurrentView("register");
+        } else {
+            setCurrentView("login");
+        }
+    }, [location.hash])
+
+    if (needConfirmation) {
+        return <Confirmation/>
+    }
+
+    return (
+        <div>
+            {currentView === "login" ? <Login /> : <Register />}
+        </div>
+    )
+}
+ export default Auth
